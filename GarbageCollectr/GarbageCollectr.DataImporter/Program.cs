a55543d8e41e9daeaf7b2b51ee0edd295c3f98d7
Ola Karlsson <olkarls@gmail.com>
2016-04-28 21:45:42 +0200

    Adds data importer project.

---
+++ GarbageCollectr/GarbageCollectr.DataImporter/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace GarbageCollectr.DataImporter
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = File.ReadAllLines(@"C:\Users\Ola_Kar\Downloads\avfallstomningar0.csv");
+
+            var row = from r in rows
+                      select (r.Split(',')).ToArray();
+
+
+
+            Console.WriteLine(row.FirstOrDefault().ElementAt(0));
+            Console.ReadLine();
+
+            // Löpnummer; Tömningsdatum; Fordonskod; Avtalskod; Avfallstyp; Händelsetyp; Avtalsbenämning; Registreringsnummer; Vikt; Enhet vikt; Postnummer
+
+
+        }
+    }
+}
