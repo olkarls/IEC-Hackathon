4c3eb930f2d9eaab5ec4628513236cf94d54b67e
Bjorn_Car <Bjorn_Car@SE-SPCS-SC-0278>
2016-04-28 23:04:34 +0200

    Added Container controller/manager

---
+++ GarbageCollectr/GarbageCollectr.Web/Business/ContainerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using GarbageCollectr.Web.Data;
+using GarbageCollectr.Web.Data.Models;
+
+namespace GarbageCollectr.Web.Business
+{
+    public class ContainerManager
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ContainerManager(ApplicationDbContext appDbContext)
+        {
+            _applicationDbContext = appDbContext;
+        }
+
+        public Container Get(Guid id)
+        {
+            return _applicationDbContext.Containers.SingleOrDefault(c => c.Id == id);
+        }
+    }
+}
---
+++ GarbageCollectr/GarbageCollectr.Web/Controllers/ContainerController.cs
@@ -0,0 +1,30 @@
+﻿namespace GarbageCollectr.Web.Controllers
+{
+    using System;
+
+    using GarbageCollectr.Web.Business;
+    using GarbageCollectr.Web.Data;
+    using Microsoft.AspNet.Http;
+    using Microsoft.AspNet.Mvc;
+    using Microsoft.Net.Http.Headers;
+    using Microsoft.WindowsAzure.Storage;
+
+    [Route("/container")]
+    public class ContainerController : Controller
+    {
+        private ApplicationDbContext DbContext;
+
+        public ContainerController(ApplicationDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get(Guid id)
+        {
+            var container = new ContainerManager(this.DbContext).Get(id);
+
+            return new JsonResult(container);
+        }
+    }
+}
