1ea516b333d75d6c4cdfe41044528b8910b0b84c
RuudieRaab <tobiasrunesson@hotmail.com>
2016-04-28 21:44:16 +0200

    file upload

--- GarbageCollectr/GarbageCollectr.Web/wwwroot/app/app.js
+++ GarbageCollectr/GarbageCollectr.Web/wwwroot/app/app.js
@@ -1,7 +1,20 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('app', [
+    var app = angular.module('app', [
+        'flow',
+        'ngRoute'
     ]);
 
+    app.config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'app/uploadFile/uploadFile.html',
+                    controller: 'uploadFileController'
+                }).
+        otherwise({
+            redirectTo: '/uploadFile'
+        });
+        }]);
 })();
